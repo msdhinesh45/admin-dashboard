1924373259e196673f5662e18e33c5c3d9be078c
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-05-19 21:03:29 +0530

    Header sections and relative icons aree added

--- admin-dashboard/src/Header.js
+++ admin-dashboard/src/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { BsFillBellFill,BsFillEnvelopeFill,BsPersonCircle,BsSearch,BsJustify, BsBellFill } from 'react-icons/bs';
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header'>
+      <div className='menu-icon'>
+        <BsJustify className='icon' />
+      </div>
+      <div className='header-left'>
+        <BsSearch className='icon' />
+      </div>
+      <div className='menu-right'>
+        <BsBellFill className='icon' />
+        <BsFillEnvelopeFill className='icon' />
+        <BsPersonCircle className='icon' />
+      </div>
+    </header>
   )
 }
 
