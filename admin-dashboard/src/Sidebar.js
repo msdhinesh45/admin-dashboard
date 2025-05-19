fc454239b4458f89a054ee55109fc659148e4c63
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-05-19 22:36:51 +0530

    Sidebar sections and Relative Icons are added

--- admin-dashboard/src/Sidebar.js
+++ admin-dashboard/src/Sidebar.js
@@ -1,8 +1,52 @@
 import React from 'react'
-
+import { BsCart3,BsGrid1X2Fill,BsFillArchiveFill,BsFillGrid3X3GapFill,BsPeopleFill,BsListCheck,BsMenuButtonWideFill,BsFillGearFill } from 'react-icons/bs';
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside>
+      <div className='sidebar-title'>
+      <div className='sidebar-brand'>
+        <BsCart3 className="header_icon" />SHOP
+      </div>
+      <span className="icon close_icon">X</span>    
+      </div>
+      <ul className='sidebar-list'>
+        <li className='sidebar-list-iten'>
+          <a href=''>
+            <BsGrid1X2Fill className='icon' />Dashboard
+          </a>
+        </li>
+        <li className='sidebar-list-iten'>
+          <a href=''>
+            <BsFillArchiveFill className='icon' />Products
+          </a>
+        </li>
+        <li className='sidebar-list-iten'>
+          <a href=''>
+            <BsFillGrid3X3GapFill className='icon' />Categories
+          </a>
+        </li>
+        <li className='sidebar-list-iten'>
+          <a href=''>
+            <BsPeopleFill className='icon' />Customers
+          </a>
+        </li>
+        <li className='sidebar-list-iten'>
+          <a href=''>
+            <BsListCheck className='icon' />Inventory
+          </a>
+        </li>
+        <li className='sidebar-list-iten'>
+          <a href=''>
+            <BsMenuButtonWideFill className='icon' />Reports
+          </a>
+        </li>
+        <li className='sidebar-list-iten'>
+          <a href=''>
+            <BsFillGearFill className='icon' />Settings
+          </a>
+        </li>
+      </ul>
+    </aside>
   )
 }
 
