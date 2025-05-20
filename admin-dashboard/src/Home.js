016e2b7ace9d23e008d8e3f6ceecd40eb4207018
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-05-20 22:45:28 +0530

    Home Components jsx parts over

--- admin-dashboard/src/Home.js
+++ admin-dashboard/src/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BsGrid1X2Fill,BsFillArchiveFill,BsFillGrid3X3GapFill,BsPeopleFill,BsListCheck,BsMenuButtonWideFill,BsFillGearFill } from 'react-icons/bs';
+import { BsFillArchiveFill,BsFillGrid3X3GapFill,BsPeopleFill,BsFillBellFill } from 'react-icons/bs';
 
 const Home = () => {
   return (
@@ -18,10 +18,24 @@ const Home = () => {
         </div>
         <div className="card">
         <div className="card-inner">
-          <h3>PRODUCTS</h3>
-          <BsFillArchiveFill className='card_icon'></BsFillArchiveFill>
+          <h3>CATEGORIES</h3>
+          <BsFillGrid3X3GapFill className='card_icon'></BsFillGrid3X3GapFill>
         </div>
-        <h1>300</h1>
+        <h1>12</h1>
+        </div>
+        <div className="card">
+        <div className="card-inner">
+          <h3>CUSTOMERS</h3>
+          <BsPeopleFill className='card_icon'></BsPeopleFill>
+        </div>
+        <h1>39</h1>
+        </div>
+        <div className="card">
+        <div className="card-inner">
+          <h3>ALERTS</h3>
+          <BsFillBellFill className='card_icon'></BsFillBellFill>
+        </div>
+        <h1>42</h1>
         </div>
       </div>
     </main>
