9e99ae1d3d6c9b6d7299251c9ed50f6518f9eb85
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-05-20 22:32:20 +0530

    New commits

--- admin-dashboard/src/Home.js
+++ admin-dashboard/src/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BsCart3,BsGrid1X2Fill,BsFillArchiveFill,BsFillGrid3X3GapFill,BsPeopleFill,BsListCheck,BsMenuButtonWideFill,BsFillGearFill } from 'react-icons/bs';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main className='main-container'>
+      <div className='main-title'>
+        <h3>DASHBOARD</h3>
+      </div>
+
+      <div className="main-cards">
+        <div className="card-inner">
+          <h3>Products</h3>
+          <></>
+        </div>
+        <h1>300</h1>
+      </div>
+    </main>
   )
 }
 
