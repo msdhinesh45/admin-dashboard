c2c5cfa5d4570205a7fd00f6f22fd71f8f2e5927
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-05-20 22:56:55 +0530

    Charts code added

--- admin-dashboard/src/Home.js
+++ admin-dashboard/src/Home.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import { BsFillArchiveFill,BsFillGrid3X3GapFill,BsPeopleFill,BsFillBellFill } from 'react-icons/bs';
+import {
+  ComposedChart,
+  Line,
+  Area,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
 
 const Home = () => {
 
@@ -85,7 +97,27 @@ const data = [
       {/* Charts section */}
       
       <div className="charts">
-
+      <ResponsiveContainer width="100%" height="100%">
+        <ComposedChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="name" scale="band" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="uv" barSize={20} fill="#413ea0" />
+          <Line type="monotone" dataKey="uv" stroke="#ff7300" />
+        </ComposedChart>
+      </ResponsiveContainer>
       </div>
     </main>
   )
