8b0f71bf696f7b4c841bfedb6899c6855c35fe90
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-05-20 22:52:56 +0530

    Charts section added and data imported for chart

--- admin-dashboard/src/Home.js
+++ admin-dashboard/src/Home.js
@@ -2,6 +2,48 @@ import React from 'react'
 import { BsFillArchiveFill,BsFillGrid3X3GapFill,BsPeopleFill,BsFillBellFill } from 'react-icons/bs';
 
 const Home = () => {
+
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 590,
+    pv: 800,
+    amt: 1400,
+  },
+  {
+    name: 'Page B',
+    uv: 868,
+    pv: 967,
+    amt: 1506,
+  },
+  {
+    name: 'Page C',
+    uv: 1397,
+    pv: 1098,
+    amt: 989,
+  },
+  {
+    name: 'Page D',
+    uv: 1480,
+    pv: 1200,
+    amt: 1228,
+  },
+  {
+    name: 'Page E',
+    uv: 1520,
+    pv: 1108,
+    amt: 1100,
+  },
+  {
+    name: 'Page F',
+    uv: 1400,
+    pv: 680,
+    amt: 1700,
+  },
+];
+
+
   return (
     <main className='main-container'>
       <div className='main-title'>
@@ -37,6 +79,13 @@ const Home = () => {
         </div>
         <h1>42</h1>
         </div>
+      </div>
+
+
+      {/* Charts section */}
+      
+      <div className="charts">
+
       </div>
     </main>
   )
